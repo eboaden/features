9b29a1c877bf93bb27e5e1f478c87531dd54d9a0
Edward Boaden <github@edwardboaden.com>
2020-07-08 01:54:40 +0100

    Updated documentation

--- documentation-site/src/App.js
+++ documentation-site/src/App.js
@@ -11,7 +11,7 @@ function App() {
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-					This is some new documentation
+					Learn React
 				</a>
 			</header>
 		</div>
