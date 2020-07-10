219b9c60714c4fc498e2f1764c17ddb6b4259701
Edward Boaden <edward@boaden.net>
2020-07-10 14:13:36 +0100

    Added inital demo app code

---
+++ examples/demo-app/RandomDataGenerator/RandomDataGenerator.Api/Controllers/RandomNameController.cs
@@ -0,0 +1,26 @@
+﻿using Bogus;
+using Microsoft.AspNetCore.Mvc;
+using Features;
+
+namespace RandomDataGenerator.Api.Controllers
+{
+    [ApiController]
+    [Route("random-name")]
+    public class RandomNameController : ControllerBase
+    {
+        private readonly FeaturesClient _featuresClient;
+        private readonly Faker _faker;
+
+        public RandomNameController(FeaturesClient featuresClient)
+        {
+            _featuresClient = featuresClient;
+            _faker = new Faker();
+        }
+        
+        [HttpGet]
+        public string Get()
+        {
+            return _featuresClient.GetFeature("change-name-to-city") ? $"City: {_faker.Address.City()}": $"Name: {_faker.Name.FullName()}";
+        }
+    }
+}
